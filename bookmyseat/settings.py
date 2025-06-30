1384e2bd3f272bf28f8876867745ad0a661736ca
Chintala Nithya Sree <your-email@example.com>
2025-06-30 14:14:20 +0530

    Clean settings.py and setup for Vercel

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -21,10 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c8aetlj(=vp90n@#yoc^&d(_6ivp(d!bv-4-f!r$lawptjzrwu'
+#SECRET_KEY = 'django-insecure-c8aetlj(=vp90n@#yoc^&d(_6ivp(d!bv-4-f!r$lawptjzrwu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-key-for-dev')
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+
+
 
 ALLOWED_HOSTS = ['*']
 
@@ -86,12 +90,36 @@ WSGI_APPLICATION = 'bookmyseat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+   # }
+#}
+# import dj_database_url
+
+# DATABASES = {
+#     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+# }
+
+
+
+
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default='postgresql://djangobook_user:bQp3AyyEqah0ZID8zxjce2JT5ayPjjl3@dpg-d194vd95pdvs73dujgr0-a.oregon-postgres.render.com/djangobook'
+#     )
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
+
+
+
 #DATABASES['default'] = dj_database_url.parse('postgresql://djangobook_user:bQp3AyyEqah0ZID8zxjce2JT5ayPjjl3@dpg-d194vd95pdvs73dujgr0-a.oregon-postgres.render.com/djangobook')
 # 
 
@@ -130,9 +158,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
