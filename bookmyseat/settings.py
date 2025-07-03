9c8f473db6484b95cf089b58a42e1e4ba68dac2b
Chintala Nithya Sree <your-email@example.com>
2025-07-03 11:15:16 +0530

    Updated for Vercel deployment with PostgreSQL and static files

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -105,18 +105,18 @@ WSGI_APPLICATION = 'bookmyseat.wsgi.application'
 
 
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://djangobook_user:bQp3AyyEqah0ZID8zxjce2JT5ayPjjl3@dpg-d194vd95pdvs73dujgr0-a.oregon-postgres.render.com/djangobook'
-    )
-}
-
-
 # DATABASES = {
 #     'default': dj_database_url.config(
-#         default=os.environ.get('DATABASE_URL')
+#         default='postgresql://djangobook_user:bQp3AyyEqah0ZID8zxjce2JT5ayPjjl3@dpg-d194vd95pdvs73dujgr0-a.oregon-postgres.render.com/djangobook'
 #     )
 # }
+
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL','postgresql://djangobook_user:bQp3AyyEqah0ZID8zxjce2JT5ayPjjl3@dpg-d194vd95pdvs73dujgr0-a.oregon-postgres.render.com/djangobook')
+    )
+}
 
 
 
