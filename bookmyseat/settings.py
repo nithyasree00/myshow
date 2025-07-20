e413c68f2396f9f2046afcf9014ae2d236a3bfc1
Chintala Nithya Sree <your-email@example.com>
2025-07-20 07:17:53 +0530

    nine commit

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -112,18 +112,18 @@ WSGI_APPLICATION = 'bookmyseat.wsgi.application'
 # }
 
 
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=os.environ.get('DATABASE_URL','postgresql://djangobook_user:bQp3AyyEqah0ZID8zxjce2JT5ayPjjl3@dpg-d194vd95pdvs73dujgr0-a.oregon-postgres.render.com/djangobook?sslmode=require')
-#     )
-# }
-
-
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
+        default=os.environ.get('DATABASE_URL','postgresql://djangobook_user:bQp3AyyEqah0ZID8zxjce2JT5ayPjjl3@dpg-d194vd95pdvs73dujgr0-a.oregon-postgres.render.com/djangobook')
     )
 }
+
+
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.environ.get('DATABASE_URL')
+#     )
+# }
 import os
 ALLOWED_HOSTS = ['pythondjango.onrender.com']
 
